2151e3325b496818721ca9d3f4b49c524158900c
charles-bezerra <charlesbezerra5@gmail.com>
2019-11-29 01:15:41 -0300

    1 bug resolvido

--- src/components/App.js
+++ src/components/App.js
@@ -8,164 +8,89 @@ var user = require('./dados/usuario.json');
 
 var vendas = [0,0,0,0,0,0,0,0,0,0,0,0];
 
+function selectCountMont(month) {
+    switch (month) {
+                case "January":
+                  vendas[0]+=1;
+                  break;
+                case "February":
+                  vendas[1]+=1;
+                  break;
+                case "March":
+                 vendas[2]+=1;
+                  break;
+                case "April":
+                 vendas[3]+=1;
+                  break;
+                case "May":
+                 vendas[4]+=1;
+                  break;
+                case "June":
+                 vendas[5]+=1;
+                  break;
+                case "July":
+                 vendas[6]+=1;
+                  break;
+                case "August":
+                 vendas[7]+=1;
+                  break;
+                case "September":
+                 vendas[8]+=1;
+                  break;
+                case "Octupber":
+                 vendas[9]+=1;
+                  break;
+                case "December":
+                 vendas[10]+=1;
+                  break;
+                case "November":
+                 vendas[11]+=1;
+                  break;
+
+                default:
+                  break;
+    }
+}
+
 class App extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {value: <Graphics vendas={vendas} data={data} />};
-        this.changeGraphicsCategory({"target": {"value": data.categories[0]}});
+        this.state = {value: ""};
     }
 
-    changeGraphicsCategory = (event) => {
+    updateGraphics(){
+        ReactDOM.render(
+          <Graphics vendas={vendas} data={data} />,
+          document.getElementById('graphics')
+        );
+    }
+
+    changeGraphicsCategory = (event) =>{
         for(let i = 0; i < data.vendas.length; i++){
-            if(data.vendas[i].category == event.target.value){
-              switch (data.vendas[i].month) {
-                case "January":
-                  vendas[0]+=1;
-                  break;
-                case "February":
-                  vendas[1]+=1;
-                  break;
-                case "March":
-                 vendas[2]+=1;
-                  break;
-                case "April":
-                 vendas[3]+=1;
-                  break;
-                case "May":
-                 vendas[4]+=1;
-                  break;
-                case "June":
-                 vendas[5]+=1;
-                  break;
-                case "July":
-                 vendas[6]+=1;
-                  break;
-                case "August":
-                 vendas[7]+=1;
-                  break;
-                case "September":
-                 vendas[8]+=1;
-                  break;
-                case "Octupber":
-                 vendas[9]+=1;
-                  break;
-                case "December":
-                 vendas[10]+=1;
-                  break;
-                case "November":
-                 vendas[11]+=1;
-                  break;
+            if(data.vendas[i].category == event.target.value)
+                selectCountMont(data.vendas[i].month);
+        } 
 
-                default:
-                  break;
-              }
-          }
-        }        
-
-        console.log("passou");
-        
-        this.setState({value: <Graphics vendas={vendas} data={data} />});
+        this.updateGraphics();       
     }
 
     changeGraphicsBrand = (event) => {
         for(let i = 0; i < data.vendas.length; i++){
-            if(data.vendas[i].brand == event.target.value){
-              switch (data.vendas[i].month) {
-                case "January":
-                  vendas[0]+=1;
-                  break;
-                case "February":
-                  vendas[1]+=1;
-                  break;
-                case "March":
-                 vendas[2]+=1;
-                  break;
-                case "April":
-                 vendas[3]+=1;
-                  break;
-                case "May":
-                 vendas[4]+=1;
-                  break;
-                case "June":
-                 vendas[5]+=1;
-                  break;
-                case "July":
-                 vendas[6]+=1;
-                  break;
-                case "August":
-                 vendas[7]+=1;
-                  break;
-                case "September":
-                 vendas[8]+=1;
-                  break;
-                case "Octupber":
-                 vendas[9]+=1;
-                  break;
-                case "December":
-                 vendas[10]+=1;
-                  break;
-                case "November":
-                 vendas[11]+=1;
-                  break;
-
-                default:
-                  break;
-              }
-          }
+            if(data.vendas[i].brand == event.target.value)
+                selectCountMont(data.vendas[i].month);
         }        
 
-        this.setState({value: <Graphics vendas={vendas} data={data} />});
+        this.updateGraphics();          
     }
     
     changeGraphicsProduct = (event) => {
         for(let i = 0; i < data.vendas.length; i++){
-            if(data.vendas[i].product == event.target.value){
-              switch (data.vendas[i].month) {
-                case "January":
-                  vendas[0]+=1;
-                  break;
-                case "February":
-                  vendas[1]+=1;
-                  break;
-                case "March":
-                 vendas[2]+=1;
-                  break;
-                case "April":
-                 vendas[3]+=1;
-                  break;
-                case "May":
-                 vendas[4]+=1;
-                  break;
-                case "June":
-                 vendas[5]+=1;
-                  break;
-                case "July":
-                 vendas[6]+=1;
-                  break;
-                case "August":
-                 vendas[7]+=1;
-                  break;
-                case "September":
-                 vendas[8]+=1;
-                  break;
-                case "Octupber":
-                 vendas[9]+=1;
-                  break;
-                case "December":
-                 vendas[10]+=1;
-                  break;
-                case "November":
-                 vendas[11]+=1;
-                  break;
-
-                default:
-                  break;
-              }
-          }
+            if(data.vendas[i].product == event.target.value)
+                selectCountMont(data.vendas[i].month);
         }        
 
-        this.setState({value: <Graphics vendas={vendas} data={data} />});
+        this.updateGraphics();       
     }
 
     getOptions(options) {
@@ -176,46 +101,46 @@ class App extends React.Component {
          for(let i = 0; i < myOptions.length; i++){
             option = myOptions[i];
             if (typeof option == 'object')
-                 children.push( <option value={option.name}>{option.name}</option> );
+                 children.push( <option value={ option.name }>{option.name}</option> );
             else
-                children.push( <option value={option}>{option}</option> );                         
+                children.push( <option value={ option }>{ option }</option> );                         
          }
          return children;
     }
 
     render() {
       return (
-        <div class="app">
+        <div className="app">
             <Header user={user} />
 
-            <div class="p-4">
+            <div className="p-4">
                 <center>
                     <h2>Sales by month</h2>
                 </center>
 
-                <div class="row w-100">
-                    <div class="col-4">
-                        <div class="form-group">
+                <div className="row w-100">
+                    <div className="col-4">
+                        <div className="form-group">
                             Categoria
-                            <select  class="form-control" onChange={this.changeGraphicsCategory}>
+                            <select  className="form-control" onChange={this.changeGraphicsCategory}>
                                 { this.getOptions(data.categories) }
                             </select>
                         </div>
                     </div>
                     
-                    <div class="col-4">
-                        <div class="form-group">
+                    <div className="col-4">
+                        <div className="form-group">
                             Produtos
-                            <select  class="form-control" onChange={this.changeGraphicsProduct}>
+                            <select  className="form-control" onChange={this.changeGraphicsProduct}>
                                 { this.getOptions(data.products) }
                             </select>
                         </div>
                     </div>
                     
-                    <div class="col-4">
-                        <div class="form-group">
+                    <div className="col-4">
+                        <div className="form-group">
                             Marcas
-                            <select  class="form-control" onChange={this.changeGraphicsBrand}>
+                            <select  className="form-control" onChange={this.changeGraphicsBrand}>
                                 { this.getOptions(data.brands) }
                             </select>
                         </div>
@@ -223,9 +148,7 @@ class App extends React.Component {
                 </div>
             </div>
             
-            <div id="graphics" class="py-2 px-4 graphics">
-                { this.state.value }
-            </div>
+            <div id="graphics" className="py-2 px-4 graphics"></div>
         </div>
       );
     }
--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 export default class Header extends Component{
     render() {
         return (
-            <div>
-                <nav class="navbar navbar-dark bg-info">
-                    <a class="navbar-brand" href="#">Menu</a>
-                    <a class="navbar-brand" href="#">{ this.props.user.name }</a>
-                    <a class="navbar-brand" href="#">Sales Report</a>
+            <div className="header">
+                <nav className="navbar navbar-dark bg-info">
+                    <a className="navbar-brand" href="#">Menu</a>
+                    <a className="navbar-brand" href="#">{ this.props.user.name }</a>
+                    <a className="navbar-brand" href="#">Sales Report</a>
                 </nav>
             </div>
         );
